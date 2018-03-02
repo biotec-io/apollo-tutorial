ef5df9d6365725ee9bb6c0510d5bf455b49b7c09
Edgar Talledos <onimenotsuki@gmail.com>
2018-03-02 14:03:55 -0600

    Update App component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const ChannelsList = () => {
-  return (
-    <ul>
+const ChannelsList = () => (
+  <ul>
     <li>Channel 1</li>
     <li>Channel 2</li>
-    </ul>
-  );
-};
+  </ul>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      <ChannelsList />
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Apollo</h2>
+        </div>
+        <ChannelsList />
       </div>
     );
   }
